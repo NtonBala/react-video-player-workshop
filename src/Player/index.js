9ef4358ef8b05d5a0ce5449358b97b0fe124909b
Anton Balashov <balashov.anton.1983@gmail.com>
2019-07-02 21:21:23 +0300

    Implement react video player

--- src/Player/index.js
+++ src/Player/index.js
@@ -1,5 +1,5 @@
 // Core
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 // Instruments
 import './styles.css';
@@ -7,20 +7,98 @@ import './styles.css';
 import video from './video.mp4';
 
 export const Player = () => {
-    return <h1>Поехали!</h1>;
+    const [ isPlaying, setIsPlaying ] = useState(false);
+    const [ progress, setProgress ] = useState(0);
+    const [ isProgressCapturing, setIsProgressCapturing ] = useState(false);
 
-    const isPlaying = false;
+    /**
+     * Создаём реф для элемента video.
+     * Реф в React — это прямой доступ к html-элементу.
+     * С его помощью мы сможем управлять видеоплеером в явном виде.
+     */
+    const videoRef = useRef(null);
+
+    /* Включаем или выключаем проигрывание видео. */
+    const togglePlay = () => {
+        const method = videoRef.current.paused ? 'play' : 'pause';
+
+        videoRef.current[ method ]();
+        setIsPlaying(method === 'play');
+    };
+
+    /* Прокручиваем прогресс проигрывания. */
+    const skip = (event) => {
+        const seconds = event.target.dataset.skip;
+
+        videoRef.current.currentTime += Number.parseFloat(seconds);
+    };
+
+    /* Устанавливаем прогресс проигранного видео в процентах. */
+    const handleProgress = () => {
+        const percent
+            = videoRef.current.currentTime / videoRef.current.duration * 100;
+
+        setProgress(percent);
+    };
+
+    /* Устанавливаем прогресс видео указателем мыши. */
+    const scrub = (event) => {
+        const scrubTime
+            = event.nativeEvent.offsetX / event.currentTarget.offsetWidth
+            * videoRef.current.duration;
+
+        videoRef.current.currentTime = scrubTime;
+    };
+
+    /* Добавляем слушатель вкл/выкл видео по нажатию на пробел. */
+    useEffect(() => {
+        const handler = (event) => {
+            if (event.code === 'Space') {
+                togglePlay();
+            }
+        };
+
+        /* Подписка, выполняется при первом рендере один раз. */
+        document.addEventListener('keydown', handler);
+
+        /* Отписка, выполняется при удалении компонента один раз. */
+        return () => document.removeEventListener('keydown', handler);
+        /* Эффект выполняется один раз, потому что вторым аргументом мы передали []. */
+    }, []);
 
     const playControl = isPlaying ? <>&#10074;&#10074;</> : <>&#9654;</>;
 
     return (
         <div className = 'player'>
-            <video src = { video } />
+
+            <video
+                ref = { videoRef }
+                src = { video }
+                onClick = { togglePlay }
+                onTimeUpdate = { handleProgress }
+            />
+
             <div className = 'controls'>
-                <div className = 'progress'>
-                    <div className = 'filled' />
+                <div
+                    className = 'progress'
+                    onClick = { scrub }
+                    onMouseDown = { () => setIsProgressCapturing(true) }
+                    onMouseMove = { (event) => isProgressCapturing && scrub(event) }
+                    onMouseUp = { () => setIsProgressCapturing(false) }>
+                    <div
+                        className = 'filled'
+                        style = {{
+                            '--filledProgressBar': `${progress}%`,
+                        }}
+                    />
                 </div>
-                <button title = 'Toggle Play'>{playControl}</button>
+
+                <button
+                    title = 'Toggle Play'
+                    onClick = { togglePlay }>
+                    {playControl}
+                </button>
+
                 <input
                     className = 'slider'
                     max = '1'
@@ -29,8 +107,18 @@ export const Player = () => {
                     step = '0.05'
                     type = 'range'
                 />
-                <button data-skip = '-10'>« 10s</button>
-                <button data-skip = '25'>25s »</button>
+
+                <button
+                    data-skip = '-10'
+                    onClick = { skip }>
+                    « 10s
+                </button>
+
+                <button
+                    data-skip = '25'
+                    onClick = { skip }>
+                    25s »
+                </button>
                 <button>&#10021;</button>
             </div>
         </div>
